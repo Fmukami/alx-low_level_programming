ae815160c7dc064d33df5cfaa514230d5c6fae53
Fmukami <faithnjuguna43@gmail.com>
2023-04-18 11:18:04 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,47 @@
+#include <stdlib.h>
 #include "dog.h"
 
-
 /**
-*_strcopy - copy string pointed by src
-*into dest variable
-*@dest:buffer storing string copy
-*@src: buffer storing string to copy
-*Return:returns copied string
-*/
-char *_strcopy(char *dest, char *src)
-{
-int index = 0;
-
-for (; src[index] ; index++)
-dest[index] = src[index];
-
-dest[index] = '\0';
-return (dest);
-}
-
-
-/**
-*new_dog - creates a new dog
-*@name: name of new dog
-*@age: age of new dog
-*@owner: owner of new dog
-*Return: returns NULL in case
-*of failure
-*/
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *poppy;
+	unsigned int nl, ol, i;
+	dog_t *dog;
 
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
-
-poppy = malloc(sizeof(dog_t));
-if (poppy == NULL)
-return (NULL);
-
-poppy->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (poppy->name == NULL)
-{
-free(poppy);
-return (NULL);
-}
-
-poppy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (poppy->owner == NULL)
-{
-free(poppy->name);
-free(poppy);
-return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
