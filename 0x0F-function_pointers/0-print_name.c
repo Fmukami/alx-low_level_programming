51c1ef041a787c9fb8b083f9ccf6d2a87a451400
Faith Njuguna <faithnjuguna43@gmail.com>
2023-08-16 13:31:14 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *@f: function pointer.
+ * print_name - prints a name.
+ * @name: name of the person.
+ * @f: function pointer.
  *
  * Return: Nothing.
  */
