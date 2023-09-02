0e11a1931c5f40f97ec18fdad2677b2b619f45db
Faith <faithnjuguna43@gmail.com>
2023-09-01 18:46:57 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int isNum(char num[])
+int main(int argc, char *argv[])
